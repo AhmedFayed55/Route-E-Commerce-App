ae7969c2e15ed24844d0aadc8f6c3dd65a597f16
fayed <anaa71137@gmail.com>
2025-07-04 16:39:54 +0300

    - Setup data & domain layers for auth feature. - Implemented abstract & concrete classes for auth. - Created custom ApiManager for API handling. - Configured all auth endpoints in ApiManager. - Implement errors file.

---
+++ lib/features/auth/domain/entity/login_response_entity.dart
@@ -0,0 +1,15 @@
+class LoginResponseEntity {
+  LoginResponseEntity({this.message, this.user, this.token, this.statusMsg});
+
+  String? message;
+  String? statusMsg;
+  UserLoginEntity? user;
+  String? token;
+}
+
+class UserLoginEntity {
+  UserLoginEntity({this.name, this.email});
+
+  String? name;
+  String? email;
+}
---
+++ lib/features/auth/presentation/widgets/login_button.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import '../../../../config/routes_manager/routes.dart';
+import '../../../../core/resources/color_manager.dart';
+import '../../../../core/resources/styles_manager.dart';
+import '../../../../core/resources/values_manager.dart';
+import '../../../../core/widgets/custom_elevated_button.dart';
+
+
+class LoginButton extends StatelessWidget {
+  const LoginButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: SizedBox(
+        child: CustomElevatedButton(
+          isStadiumBorder: false,
+          label: 'Login',
+          backgroundColor: ColorManager.white,
+          textStyle: getBoldStyle(
+            color: ColorManager.primary,
+            fontSize: AppSize.s18,
+          ),
+          onTap: () {
+            Navigator.pushNamedAndRemoveUntil(
+                context, Routes.mainRoute, (route) => false);
+          },
+        ),
+      ),
+    );
+  }
+}
